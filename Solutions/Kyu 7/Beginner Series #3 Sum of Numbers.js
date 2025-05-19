ef80351014b6efe5025a82303dd0b3e008e653aa
Zelchi <tininhakawaii@gmail.com>
2025-05-19 11:02:21 -0300

    Beginner Series #3 Sum of Numbers

---
+++ Solutions/Kyu 7/Beginner Series #3 Sum of Numbers.js
@@ -0,0 +1,14 @@
+// Dado dois números inteiros a e b, que podem ser positivos ou negativos, encontre a soma de todos os inteiros entre eles, incluindo a e b, e retorne esse valor.
+// Se os dois números forem iguais, retorne a ou b.
+
+function getSum(a, b) {
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+    let result = 0;
+
+    for (let i = min; i <= max; i++) {
+        result += i;
+    }
+
+    return result;
+}
